c3fa99ca843269f80084f54380357af854e775ae
a-razhenkova <a.razhenkova@gmail.com>
2026-05-06 22:58:26 +0300

    added unit tests for pdkdf2

---
+++ Tests/UnitTests/Pbkdf2KeyTests.cs
@@ -0,0 +1,41 @@
+﻿using Application;
+using FluentAssertions;
+using Xunit.Priority;
+
+namespace UnitTests
+{
+    public class Pbkdf2KeyTests
+    {
+        [Priority(1)]
+        [Theory]
+        [InlineData("m4A0?Edis66a", 100_000, 128, 16)]
+        public void CreateHash_RecreateSameHash(string password, int interactions, int hashLength, int saltLength)
+        {
+            // Arrange
+            (string hash, string salt) = Pbkdf2Key.Create(password, interactions, hashLength, saltLength);
+
+            // Act
+            (string newHash, string newSalt) = Pbkdf2Key.Recreate(password, salt, interactions, hashLength);
+
+            // Assert
+            hash.Should().Be(newHash);
+            salt.Should().Be(newSalt);
+        }
+
+        [Priority(2)]
+        [Theory]
+        [InlineData("m4A0?Edis66a", 100_000, 128, 16)]
+        public void HashUniquenessTest(string password, int interactions, int hashLength, int saltLength)
+        {
+            // Arrange
+            (string hash, string salt) = Pbkdf2Key.Create(password, interactions, hashLength, saltLength);
+
+            // Act
+            (string newHash, string newSalt) = Pbkdf2Key.Create(password, interactions, hashLength, saltLength);
+
+            // Assert
+            hash.Should().NotBe(newHash);
+            salt.Should().NotBe(newSalt);
+        }
+    }
+}
